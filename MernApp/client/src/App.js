eb9f89028d6b40a8b7b1c3dbb6b987e588a6db4e
Ahmer Bin Sajid <ahmerbinsajid07@gmail.com>
2021-06-20 21:21:36 +0500

    errors fised

--- MernApp/client/src/App.js
+++ MernApp/client/src/App.js
@@ -1,21 +1,14 @@
 import { Route, Switch} from "react-router-dom";
-import ComponentA from "./Components/Test/ComponentA";
 import Driver from './Driver'
 import React, { Component } from 'react';
-import ComponentB from "./Components/Test/ComponentB";
-
 class App extends Component {
-  
   render() {
     // console.log("A render")
     return (
       <div>
-        {/* <ComponentB/> */}
          <Driver/>
-      {/* <ComponentA/> */}
       </div>
     );
   }
 }
-
 export default App;
